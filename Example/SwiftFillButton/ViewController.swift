db0158ddf1b588aaa8cc309f1a6e3c7a1f424856
Kemal Hasan Atay <kha26@cornell.edu>
2018-07-12 10:30:12 +0300

    Initial commit

---
+++ Example/SwiftFillButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftFillButton
+//
+//  Created by kha26 on 07/12/2018.
+//  Copyright (c) 2018 kha26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
